6bdc87a53625b875a5b4ce102dea026477c08d1d
Georgie Horth <georgiehorth@gmail.com>
2021-03-16 13:32:54 +0000

    Created a new map called VolcanoesMap which has data points for Volcanoes in USA, markers not updated

--- volcanoes.py
+++ volcanoes.py
@@ -5,7 +5,7 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 
-map = folium.Map(location=[45.98, 9.21], zoom_start=10, tiles = "Stamen Terrain")
+map = folium.Map(location=[38.95, -104.94], zoom_start=5, tiles = "Stamen Terrain")
 
 
 # this is the way to add a child. However, for more control we want to use feature groups.
@@ -13,8 +13,10 @@ map = folium.Map(location=[45.98, 9.21], zoom_start=10, tiles = "Stamen Terrain"
 
 fg = folium.FeatureGroup(name="My Map")
 # to add multiple we have to add them one at a time... helloo for loops!
-fg.add_child(folium.Marker(location=[46.00, 9.18], popup="I'm a Marker", icon=folium.Icon(color="green")))
+# seeing as we're iterating over 2 lists at the same time we need to use zip!
+for lat, lon in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lat, lon], popup="I'm a Marker", icon=folium.Icon(color="green")))
 map.add_child(fg)
 
 
-map.save("Map1.html")
+map.save("VolcanoesMap.html")
