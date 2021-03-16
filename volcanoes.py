3edb2b923dae7e87e29cd6d90fef0bf3dd8101ac
Georgie Horth <georgiehorth@gmail.com>
2021-03-16 13:56:35 +0000

    Markers now state the name and the elevation of the volcano

--- volcanoes.py
+++ volcanoes.py
@@ -4,6 +4,8 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
 
 map = folium.Map(location=[38.95, -104.94], zoom_start=5, tiles = "Stamen Terrain")
 
@@ -14,9 +16,11 @@ map = folium.Map(location=[38.95, -104.94], zoom_start=5, tiles = "Stamen Terrai
 fg = folium.FeatureGroup(name="My Map")
 # to add multiple we have to add them one at a time... helloo for loops!
 # seeing as we're iterating over 2 lists at the same time we need to use zip!
-for lat, lon in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lat, lon], popup="I'm a Marker", icon=folium.Icon(color="green")))
+for lat, lon, name, el in zip(lat, lon, name, elev):
+    fg.add_child(folium.Marker(location=[lat, lon], popup=str(name)+" "+str(el)+" m", icon=folium.Icon(color="green")))
 map.add_child(fg)
 
 
 map.save("VolcanoesMap.html")
+
+# popup=folium.Popup(str(el), parse_html=True) this is if the string you're passing has ' in them
