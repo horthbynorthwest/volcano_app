277f709b387f8cf4bb609e9e563b6579a03ed78b
Georgie Horth <georgiehorth@gmail.com>
2021-03-16 14:04:40 +0000

    Added colour_producer method which has a conditional to set colour of markers based on elevation

--- volcanoes.py
+++ volcanoes.py
@@ -7,17 +7,24 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
 
-map = folium.Map(location=[38.95, -104.94], zoom_start=5, tiles = "Stamen Terrain")
+def colour_producer(elev):
+    if elev < 1500:
+        return 'green'
+    elif 1500<= elev < 2500:
+        return 'orange'
+    else:
+        return 'red' 
 
+map = folium.Map(location=[38.95, -104.94], zoom_start=5, tiles = "Stamen Terrain")
 
 # this is the way to add a child. However, for more control we want to use feature groups.
 # map.add_child(folium.Marker(location=[46.00, 9.18], popup="I'm a Marker", icon=folium.Icon(color="green")))
 
 fg = folium.FeatureGroup(name="My Map")
 # to add multiple we have to add them one at a time... helloo for loops!
-# seeing as we're iterating over 2 lists at the same time we need to use zip!
+# seeing as we're iterating over more than one list at the same time we need to use zip!
 for lat, lon, name, el in zip(lat, lon, name, elev):
-    fg.add_child(folium.Marker(location=[lat, lon], popup=str(name)+" "+str(el)+" m", icon=folium.Icon(color="green")))
+    fg.add_child(folium.Marker(location=[lat, lon], popup=str(name)+" "+str(el)+" m", icon=folium.Icon(color=colour_producer(el))))
 map.add_child(fg)
 
 
